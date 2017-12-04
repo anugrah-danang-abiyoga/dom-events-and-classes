dfa7fec3bdad7823cd77e498f8d809ed3778b5cc
anugrah-danang-abiyoga <danangabiyoga26@gmail.com>
2017-12-04 16:52:56 +1300

    initial commit

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
@@ -18,23 +18,43 @@ function bindEventListeners (dots) {
 function makeGreen (evt) {
   evt.preventDefault()
   evt.target.classList.toggle('green')
+  evt.target.classList.remove('blue')
   updateCounts()
 }
 
 // CREATE FUNCTION makeBlue HERE
-
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  evt.target.classList.remove('green')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
-
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 function updateCounts () {
   var totals = {
     blue: 0,
     green: 0,
     invisible: 0
   }
-  
-  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
 
-  // Once you've done the counting, this function will update the display
+  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+let dots = document.getElementsByClassName('board')[0].children
+  // Once you've done the counting, this function will update the display. This is traditional loop
+if (dot.classList.contains('green')) {
+  total green += 1;
+}
+
+if (dot.classList.contains('blue')) {
+  total blue += 1;
+}
+
+if (dot.classList.contains('invisible')) {
+  total invisible += 1;
+}
+
   displayTotals(totals)
 }
 
